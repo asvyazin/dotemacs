5e5dc473c70e99bce2ddef7aaa0465fee0d1a6a3
Alexander Svyazin <guybrush@live.ru>
2012-12-10 09:26:41 +0600

    paredit-mode

--- init.el
+++ init.el
@@ -3,3 +3,11 @@
 ;; egg-mode
 (add-to-list 'load-path "~/.emacs.d/lisp/egg")
 (autoload 'egg-status "egg" "Egg git mode." t)
+
+;; paredit
+(add-to-list 'load-path "~/.emacs.d/lisp/paredit")
+(autoload 'paredit-mode "paredit" "Paredit mode." t)
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
