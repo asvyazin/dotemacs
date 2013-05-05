691182b65aa7da2e561a34df5af227d7868ee190
Alexander Svyazin <guybrush@live.ru>
2013-05-05 23:55:52 +0600

    paredit -> smartparens

--- init.el
+++ init.el
@@ -4,13 +4,12 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/egg")
 (autoload 'egg-status "egg" "Egg git mode." t)
 
-;; paredit
-(add-to-list 'load-path "~/.emacs.d/lisp/paredit")
-(autoload 'paredit-mode "paredit" "Paredit mode." t)
-(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
-(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
-(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
-(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+;; smartparens
+(add-to-list 'load-path "~/.emacs.d/lisp/dash")
+(add-to-list 'load-path "~/.emacs.d/lisp/smartparens")
+(require 'smartparens-config)
+(sp-use-smartparens-bindings)
+(smartparens-global-mode t)
 
 ;; smex
 (add-to-list 'load-path "~/.emacs.d/lisp/smex")
