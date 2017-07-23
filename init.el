3eff6d16e7fea23aa0b40cd68ed33536ca6308a8
Alexander Svyazin <guybrush@live.ru>
2017-07-23 14:37:38 +0500

    cleanup

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
     ("ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" "aed73c6d0afcf2232bb25ed2d872c7a1c4f1bda6759f84afc24de6a1aec93da8" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "6ebb2401451dc6d01cd761eef8fe24812a57793c5ccc427b600893fa1d767b1d" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "c56d90af9285708041991bbc780a073182cbe91721de17c6e7a8aac1380336b3" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "63fa843551cceac4958cc0e40e5e36c1ff79e39e1a4916017c225aca677de9d0" "0f68a45e03a507a51ae48c8fb5ec023254f972ead4d04d5813728d68f32970cc" "57cd4c79d719b74810f584d2afbbdb221334ab72077d50a875ce13ec54beadce" "e989d10979a9bd3230077945a8169cf6988cbc7ca7ca95fc7dfe373d573eea85" "ad665bdc1e81c420413cb105602ecb80c7b1f8d98a85b88164227d268c4f4f88" "2529be337b331d168deeed5a7fc257a62e5f252436f76851a30fb1b63e556e18" "01e169ce874985c2aa4ce4e3a3d5dbd6060d61457500120a557546d338901398" "134101847bd0a2ae22c72cbfda59cddf1039f9bb2b71e5ffd704feed5b409cfc" "c5a1d8f69dbc97c0158885b3c9e178a1bc75189745207c382c2d19dcf1dc91dd" "0268338ad854c131bafdecb4dbc8d3d60332e69a46e7639fd7929b4b5ef31ed2" default)))
  '(package-selected-packages
    (quote
-    (company-nixos-options avy scala-mode js2-refactor ess org-mode factor-mode purescript-mode mmm-mode ensime use-package clojure-mode-extra-font-locking flycheck-purescript idris-mode toml-mode browse-kill-ring cider deferred elm-mode git-commit haskell-snippets hydra js2-mode magit-popup psc-ide s which-key with-editor ansi auto-complete cask dash epl f flycheck flymake-go ghc names package-build pkg-info popup shut-up spinner yasnippet company haskell-mode rich-minority slime yaml-mode wrap-region virtualenv speed-type smex smartparens smart-mode-line slime-company shm scion rust-mode pymacs protobuf-mode projectile popwin paradox pallet nose nix-mode multiple-cursors markdown-mode magit livescript-mode iedit idomenu ido-vertical-mode highlight-indentation hi2 hamlet-mode god-mode go-snippets go-mode go-autocomplete fuel framemove flymake-yaml flymake-elixir flycheck-rust flycheck-hdevtools flycheck-haskell flycheck-cask flx-ido espuds ert-runner erlang elnode elixir-yasnippets elixir-mode edit-server drag-stuff discover-my-major company-go company-ghc company-cabal clojure-snippets clojure-mode base16-theme async alchemist aggressive-indent ace-jump-zap ace-jump-buffer)))
+    (company-nixos-options vdiff-magit vdiff avy scala-mode js2-refactor ess org-mode factor-mode purescript-mode mmm-mode ensime use-package clojure-mode-extra-font-locking flycheck-purescript idris-mode toml-mode browse-kill-ring cider deferred elm-mode git-commit haskell-snippets hydra js2-mode magit-popup psc-ide s which-key with-editor ansi auto-complete cask dash epl f flycheck flymake-go ghc names package-build pkg-info popup shut-up spinner yasnippet company haskell-mode rich-minority slime yaml-mode wrap-region virtualenv speed-type smex smartparens smart-mode-line slime-company shm scion rust-mode pymacs protobuf-mode projectile popwin paradox pallet nose nix-mode multiple-cursors markdown-mode magit livescript-mode iedit idomenu ido-vertical-mode highlight-indentation hi2 hamlet-mode god-mode go-snippets go-mode go-autocomplete fuel framemove flymake-yaml flymake-elixir flycheck-rust flycheck-hdevtools flycheck-haskell flycheck-cask flx-ido espuds ert-runner erlang elnode elixir-yasnippets elixir-mode edit-server drag-stuff discover-my-major company-go company-ghc company-cabal clojure-snippets clojure-mode base16-theme async alchemist aggressive-indent ace-jump-zap ace-jump-buffer)))
  '(paradox-automatically-star nil)
  '(paradox-github-token t))
 (custom-set-faces
@@ -99,11 +99,9 @@
 (use-package elixir-mode
   :mode ("\\.ex$" . elixir-mode))
 (use-package alchemist
-  :config
-  (add-hook
-   'elixir-mode-hook
-   (lambda ()
-     (alchemist-mode 1))))
+  :commands (alchemist-mode)
+  :init
+  (add-hook 'elixir-mode-hook 'alchemist-mode))
 
 ;; haskell-mode
 (use-package shm
@@ -154,7 +152,7 @@
 
 ;; god-mode
 (use-package god-mode
-  :bind ("<escape>" . god-local-mode)) 
+  :bind ("<escape>" . god-local-mode))
 
 ;; edit-server
 (use-package edit-server
@@ -177,8 +175,9 @@
 
 ;; aggressive-indent-mode
 (use-package aggressive-indent
-  :config
-  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode))
+  :commands (aggressive-indent-mode)
+  :init
+  (add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode))
 
 ;; speed-type
 (use-package speed-type
@@ -187,7 +186,7 @@
 ;; smart-mode-line
 (use-package smart-mode-line
   :config
-  (sml/setup)) 
+  (sml/setup))
 
 ;; magit
 (use-package magit
@@ -211,12 +210,12 @@
    (signal (car e) (cdr e))))\""))
    `(lambda (output)
       (if (string-match "-OK-" output)
-          (when ,(called-interactively-p 'any)
-            (message "All is well"))
-        (switch-to-buffer-other-window "*startup error*")
-        (delete-region (point-min) (point-max))
-        (insert output)
-        (search-backward "ERROR!")))))
+	  (when ,(called-interactively-p 'any)
+	    (message "All is well"))
+	(switch-to-buffer-other-window "*startup error*")
+	(delete-region (point-min) (point-max))
+	(insert output)
+	(search-backward "ERROR!")))))
 
 ;; mmm-mode
 (use-package mmm-mode
@@ -252,17 +251,21 @@
 ;; elm-mode
 (use-package elm-mode
   :mode ("\\.elm$" . elm-mode)
-  :config
-  (add-hook 'elm-mode-hook #'elm-oracle-setup-completion))
+  :init
+  (add-hook 'elm-mode-hook 'elm-oracle-setup-completion))
 
 ;; purescript
 (use-package purescript-mode
   :mode ("\\.purs$" . purescript-mode)
-  :config
-  (require 'haskell-mode)
-  (add-hook 'purescript-mode-hook 'turn-on-haskell-indentation))
+  :init
+  (add-hook
+   'purescript-mode-hook
+   (lambda ()
+     (require 'haskell-mode)
+     (turn-on-haskell-indentation))))
 (use-package psc-ide
-  :config
+  :commands (psc-ide-mode)
+  :init
   (add-hook 'purescript-mode-hook 'psc-ide-mode)
   (add-hook 'psc-ide-mode-hook 'flycheck-mode)
   (add-hook 'psc-ide-mode-hook 'psc-ide-flycheck-setup))
@@ -273,8 +276,11 @@
 	 ("\\.sbt$" . sbt-mode)))
 (use-package ensime
   :pin melpa-stable
-  :config
-  (add-hook 'scala-mode-hook 'ensime-mode))
+  :commands (ensime-mode)
+  :init
+  (add-hook 'scala-mode-hook (lambda ()
+			       (require 'ensime)
+			       (ensime-mode))))
 
 (use-package paradox
   :commands (paradox-list-packages))
@@ -293,8 +299,9 @@
 	 ("\\.jsx$" . js2-jsx-mode)))
 (use-package js2-refactor
   :defer t
+  :commands (js2-refactor-mode)
   :init
-  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (add-hook 'js2-mode-hook 'js2-refactor-mode)
   (js2r-add-keybindings-with-prefix "C-c C-m"))
 
 (use-package which-key
