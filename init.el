25ef7b5758fbfd2bc9f47964d716be60b6134997
Alexander Svyazin <guybrush@live.ru>
2017-09-30 12:37:16 +0500

    remove agda-mode

--- init.el
+++ init.el
@@ -19,8 +19,7 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(agda2-highlight-level (quote interactive))
+ ;; If there is more than one, they won't work right. 
  '(custom-enabled-themes (quote (smart-mode-line-dark base16-solarized-dark)))
  '(custom-safe-themes
    (quote
@@ -311,5 +310,3 @@
 (use-package protobuf-mode
   :mode "\\.proto$")
 
-(load-file (let ((coding-system-for-read 'utf-8))
-             (shell-command-to-string "agda-mode locate")))
