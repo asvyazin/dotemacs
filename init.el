e5790c466c94521f7b05885369fa2e4019a4cab4
Alexander Svyazin <guybrush@live.ru>
2017-12-13 09:14:45 +0500

    fixes for console emacs

--- init.el
+++ init.el
@@ -1,9 +1,13 @@
 (setq select-enable-clipboard t)
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
 (setq-default indent-tabs-mode nil)
 
 (require 'package)
